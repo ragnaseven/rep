bafae9e4d03b22e3f70734d9c75ab5ed9e537445
ragnaseven <60230809+ragnaseven@users.noreply.github.com>
2020-01-23 13:26:59 -0300

    Add files via upload

---
+++ Consulta CNPJ.py
@@ -0,0 +1,44 @@
+import requests
+
+def main():
+    print('####################')
+    print('### Consulta CEP ###')
+    print('####################\n')
+
+    input_cnpj = input('Digite seu CNPJ: ')
+
+#Gera erro caso a quantidade de digitos seja diferente de 14
+    if len(input_cnpj) != 14:
+        print('quantidade de digitos invalida\n')
+        main()
+
+#Utiliza a biblioteca da receita para realizar a busca
+    request = requests.get('https://www.receitaws.com.br/v1/cnpj/{}'.format(input_cnpj))
+    address_data = request.json()
+
+#Caso não exista erro na pesquisa, seram retornados os dados referentes ao cnpj
+    if 'CNPJ inválido' not in address_data:
+        print('**CNPJ encontrado**')
+        print('CNPJ: {}'.format(address_data['cnpj']))
+        print('Razão social: {}'.format(address_data['nome']))
+        print('Status: {}'.format(address_data['status']))
+        print('Tipo: {}'.format(address_data['tipo']))
+        print('Abertura: {}'.format(address_data['abertura']))
+        print('Atividade principal: {}'.format(address_data['atividade_principal']))
+        print('CEP: {}'.format(address_data['cep']))
+        print('Bairro: {}'.format(address_data['bairro']))
+        print('Municipio: {}'.format(address_data['municipio']))
+#Caso o cnpj esteja errado retornara "CNPJ INVÁLIDO
+    else:
+        print('CNPJ INVáLIDO')
+
+#Ao terminar a consulta sera perguntado se você deseja realizar outra
+    option = int(input('Deseja realizar uma nova consulta? \n1. Sim \n2. Não \n'))
+    if option == 1:
+        main()
+    else:
+        print("**Saindo**")
+        exit()
+
+if __name__ == '__main__':
+    main()
